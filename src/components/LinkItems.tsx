0dcaacf82347b62b90e4f1ffd6a2d1ddc47f8599
codinci <dkibau@gmail.com>
2023-10-21 12:20:14 +0300

    wrapped link text on mobile devices

--- src/components/LinkItems.tsx
+++ src/components/LinkItems.tsx
@@ -22,29 +22,44 @@ export const LinkItems = () => {
 				{linkObjects
 					.sort((a, b) => b.timeStamp - a.timeStamp)
 					.map((link, index) => (
-					<div
-						key={index}
-						className="bg-white py-4 rounded-md flex flex-col space-y-2 md:flex-row md:items-center md:justify-between"
-					>
-						<p className="mx-4 py-2 md:flex">{link.url}</p>
-						<hr className="w-full border-1 border-neutral-gray-violet md:hidden"></hr>
-						<div className="flex flex-col gap-2 md:flex-row md:items-center">
-						<p className="mx-4 py-2 text-primary-cyan">{link.shortUrl}</p>
-						<button
-							className={`${isClicked[index] ? 'bg-primary-dark-violet' : 'bg-primary-cyan'}
-							mx-2 py-4 font-semibold text-white rounded-lg md:px-8 md:py-2`}
-							onClick={() => handleLinkCopy(link.shortUrl, index)}
+						<div
+							key={index}
+							className="bg-white py-4 rounded-md flex flex-col space-y-2 md:flex-row md:items-center md:justify-between"
 						>
-							{isClicked[index] ? "Copied!" : "Copy"}
-						</button>
+							<p
+								className="mx-4 py-2 text-ellipsis overflow-hidden whitespace-nowrap
+								md:flex "
+							>
+								{link.url}
+							</p>
+
+							<hr className="w-full border-1 border-neutral-gray-violet md:hidden"></hr>
+
+							<div className="flex flex-col gap-2 md:flex-row md:items-center">
+								<p
+									className="mx-4 py-2 text-primary-cyan text-ellipsis overflow-hidden
+									whitespace-nowrap"
+								>
+									{link.shortUrl}
+								</p>
+
+								<button
+									className={`${isClicked[index] ? 'bg-primary-dark-violet' : 'bg-primary-cyan'}
+									mx-2 py-4 font-semibold text-white rounded-lg md:px-8 md:py-2`}
+									onClick={() => handleLinkCopy(link.shortUrl, index)}
+								>
+									{isClicked[index] ? "Copied!" : "Copy"}
+								</button>
+							</div>
 						</div>
-					</div>
-					))}
+					))
+				}
 				</div>
 			) : (
-				<p className="text-center text-secondary-red text-lg mt-24">
-				You haven't entered any links yet.
-				</p>
+
+					<p className="text-center text-secondary-red text-lg mt-24">
+						You haven't entered any links yet.
+					</p>
 			)}
 		</div>
 	);
