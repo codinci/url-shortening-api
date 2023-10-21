263cf0b1c1ea8e4c8230777725b8f4c7c54ac26f
codinci <dkibau@gmail.com>
2023-10-21 18:57:51 +0300

    Aligning text in HeroSection on larger screens

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -9,21 +9,21 @@ export const HeroSection = () => {
 		 	md:justify-end md:my-8`}
 		>
 			<div
-				className="flex flex-col items-center md:items-start md:my-12 md:ml-8 lg:ml-2
+				className="flex flex-col items-center md:items-start md:my-12 md:ml-8 lg:ml-6 xl:ml-2
 				lg:max-w-2xl"
 			>
 				<h1 className='text-neutral-very-dark-blue text-center font-black text-4xl my-4
-					md:text-5xl md:ml-8 md:text-start lg:ml-2 lg:text-7xl lg:leading-16 '
+					md:text-5xl md:ml-8 md:text-start lg:ml-20 xl:ml-2 lg:text-7xl lg:leading-16 '
 				>
 					More than just shorter links
 				</h1>
 				<p className='text-neutral-gray text-center font-bold max-w-md text-lg px-4 md:ml-4
-				 	md:text-start md:whitespace-normal md:text-md lg:ml-0 lg:text-xl lg:max-w-xl'>
+				 	md:text-start md:whitespace-normal md:text-md lg:ml-16 xl:ml-0 lg:text-xl lg:max-w-xl'>
 					Build your brand’s recognition and get detailed insights on how your links are performing.
 				</p>
 				<button
 					className="bg-primary-cyan text-white rounded-full px-6 py-4 font-bold mt-8
-					md:ml-8 lg:ml-4 transition-colors duration-400 ease-in-out hover:bg-[#67e8f9]"
+					md:ml-8 lg:ml-20 xl:ml-4 transition-colors duration-400 ease-in-out hover:bg-[#67e8f9]"
 				>
 					Get Started
 				</button>
